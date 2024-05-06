b5d38fd9f69e9e36e147d9a4371226422a1bf4ff
Anshul <anshulporwal6068@gmail.com>
2024-05-06 11:17:34 +0530

    add backend url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-const BASE_URL="http://localhost:4000/api/v1"
+const BASE_URL="https://fliprassignment.onrender.com/api/v1"
 const Login = () => {
   const navigate=useNavigate();
     const [formData, setFormData] = useState({
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
-const BASE_URL="http://localhost:4000/api/v1";
+const BASE_URL="https://fliprassignment.onrender.com/api/v1";
 const Signup = () => {
     const navigate = useNavigate()
     const [formData,setFormData]=useState({
